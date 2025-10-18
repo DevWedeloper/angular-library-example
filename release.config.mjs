e82bbdd73bf2d616681d39a5fff209fcbbb67c8c
DevWedeloper <vicnathangabrielle@gmail.com>
2025-10-18 22:26:50 +0800

    feat: configure semantic release

---
+++ release.config.mjs
@@ -0,0 +1,34 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+
+export default {
+  branches: ['main'],
+  preset: 'conventionalcommits',
+  presetConfig: {
+    types: [
+      { type: 'feat', section: 'Features' },
+      { type: 'fix', section: 'Bug Fixes' },
+      { type: 'chore', section: 'Chores' },
+      { type: 'docs', hidden: true },
+      { type: 'style', hidden: true },
+      { type: 'refactor', section: 'Refactoring' },
+      { type: 'perf', hidden: true },
+      { type: 'test', hidden: true },
+    ],
+  },
+  releaseRules: [{ type: 'refactor', release: 'patch' }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', { 
+      pkgRoot: './dist/ui-button',
+      tarballDir: 'dist'
+    }],
+    ['@semantic-release/git', {
+      assets: ['projects/ui-button/package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]'
+    }],
+    '@semantic-release/github'
+  ]
+};
