22991c0dff95763dd17fb40b5030b76b6183048b
DevWedeloper <vicnathangabrielle@gmail.com>
2025-10-18 23:16:07 +0800

    revert: changes

--- release.config.mjs
+++ release.config.mjs
@@ -19,12 +19,8 @@ export default {
   },
   releaseRules: [{ type: 'refactor', release: 'patch' }],
   plugins: [
-    ['@semantic-release/commit-analyzer', {
-      path: 'projects/ui-button'
-    }],
-    ['@semantic-release/release-notes-generator', {
-      path: 'projects/ui-button'
-    }],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     ['@semantic-release/npm', { 
       pkgRoot: './dist/ui-button',
       tarballDir: 'dist'
